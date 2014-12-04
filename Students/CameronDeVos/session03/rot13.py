ec624c0076ccb8164f9f23cc8e8f37e697ffb019
Cameron de Vos <cameron2@outlook.com>
2014-12-03 17:14:00 -0800

    Add second longer function that encodes text with a rot-13 cypher

--- Students/CameronDeVos/session03/rot13.py
+++ Students/CameronDeVos/session03/rot13.py
@@ -8,11 +8,12 @@ import codecs
 
 
 def rot13_encrypt(text):
+    """Take a text statement and return it with a rot-13 cypher"""
     return codecs.encode(text, "rot-13")
 
 
 def rot13_longencrypt(text):
-    text = text.lower()
+    """Take in a text statement and return it with a rot-13 cypher"""
     code = ""
     for character in text:
         if character in "abcdefghijklmnopqrstuvwxyz":
@@ -21,6 +22,21 @@ def rot13_longencrypt(text):
             if num > ord("z"):
                 num -= 26
             code = code + chr(num)
+        elif character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            num = ord(character)
+            num += 13
+            if num > ord("Z"):
+                num -= 26
+            code = code + chr(num)
         else:
             code = code + character
     return code
+
+# Test both functions for whitespace, punctuation, and capitalization
+if __name__ == '__main__':
+    assert rot13_encrypt("hello") == "uryyb"
+    assert rot13_encrypt("I'm m@king Cookies!") == "V'z z@xvat Pbbxvrf!"
+    assert rot13_longencrypt("hello") == "uryyb"
+    assert rot13_longencrypt("I'm m@king Cookies!") == "V'z z@xvat Pbbxvrf!"
+
+    print ("All Tests Pass")
