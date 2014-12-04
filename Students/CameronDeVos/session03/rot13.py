050dbd6d2ad60956b9b6c0cc0ed411b2f97f664b
Cameron de Vos <cameron2@outlook.com>
2014-12-03 16:54:34 -0800

    Add function that encodes text with a rot-13 cypher

--- Students/CameronDeVos/session03/rot13.py
+++ Students/CameronDeVos/session03/rot13.py
@@ -9,3 +9,18 @@ import codecs
 
 def rot13_encrypt(text):
     return codecs.encode(text, "rot-13")
+
+
+def rot13_longencrypt(text):
+    text = text.lower()
+    code = ""
+    for character in text:
+        if character in "abcdefghijklmnopqrstuvwxyz":
+            num = ord(character)
+            num += 13
+            if num > ord("z"):
+                num -= 26
+            code = code + chr(num)
+        else:
+            code = code + character
+    return code
