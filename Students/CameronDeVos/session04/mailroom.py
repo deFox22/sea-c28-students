cad6a60bb35f64b59030aa8937b03e45cbfc0bed
Cameron de Vos <cameron2@outlook.com>
2014-12-08 18:24:27 -0800

    Add dictionary of donors and functions to print names, add a new donor, and update a existing donor

--- Students/CameronDeVos/session04/mailroom.py
+++ Students/CameronDeVos/session04/mailroom.py
@@ -19,3 +19,23 @@ def float_check(amount):
         return True
     except ValueError:
         return False
+
+
+# Create donor database
+donorbase = {u"Tony Tiger": [88.00, 22.19],
+             u"Melody Mandy": [23.00]}
+
+
+def print_names(database):
+    print (u"Donors:\n")
+    for key in database:
+        print (key)
+    print (u"\n")
+
+
+def add_donor(name, amount=0):
+    donorbase[name] = [amount]
+
+
+def update_donor(name, amount):
+    donorbase[name].append(amount)
