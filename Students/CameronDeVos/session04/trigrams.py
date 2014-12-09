4ab63173dc412615acd52e53f4d519f21edeb482
Cameron de Vos <cameron2@outlook.com>
2014-12-08 16:58:27 -0800

    Add function that creates a word list from txt file

--- Students/CameronDeVos/session04/trigrams.py
+++ Students/CameronDeVos/session04/trigrams.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 
+from __future__ import print_function
 import io
 import string
 import random
 
 sherlock = io.open('short_sherlock.txt', encoding='utf-8')
 sherlock_data = sherlock.read()
+
+
+def create_word_list(txtfile):
+    u"""Return a list of words from the input txtfile"""
+    words = []
+    for word in txtfile.split():
+        word = word.strip(string.punctuation + string.whitespace)
+        words.append(word)
+    return words
