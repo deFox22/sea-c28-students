d0e605da211b1f4ceba1b8a360f18e7a6a767138
Cameron de Vos <cameron2@outlook.com>
2014-12-08 18:25:13 -0800

    Add main_menu function that directs user to other functions based on input

--- Students/CameronDeVos/session04/mailroom.py
+++ Students/CameronDeVos/session04/mailroom.py
@@ -39,3 +39,26 @@ def add_donor(name, amount=0):
 
 def update_donor(name, amount):
     donorbase[name].append(amount)
+# Start of user interaction
+
+
+def main_menu():
+    # TODO: Add docstring and ability to quit!
+    reply = None
+    while reply is None:
+        reply = safe_input(u"\nPlease select from the following:\n"
+                           "To send a thank you type '1'\nTo "
+                           "create a report type '2'\n"
+                           "To exit type 'q'\n"
+                           "--->  ")
+        if reply == 'q':
+            print (u"Exiting the script")
+        elif reply == '1':
+            print (u'Thank You Creation Wizard:')
+            thank_you_name()
+        elif reply == '2':
+            print (u'creating report....')
+            create_report()
+        else:
+            print (u'Sorry that is an invalid answer')
+            main_menu()
