3eb1cb0fba6faab015ca65d38640c7d7b0dd6a34
Cameron de Vos <cameron2@outlook.com>
2014-12-08 17:29:44 -0800

    Add docstring to safe_input function

--- Students/CameronDeVos/session04/safe_input.py
+++ Students/CameronDeVos/session04/safe_input.py
@@ -1,6 +1,7 @@
 
 
 def safe_input(prompt):
+    """Return None when response to prompt is EOFError or KeyboardInterrupt"""
     try:
         response = raw_input(prompt)
     except (KeyboardInterrupt, EOFError):
