666e1f7e67218e3a5434d4025202840756a709d3
Cameron de Vos <cameron2@outlook.com>
2014-12-10 08:18:40 -0800

    Add a dictionary of numbers from 0 to 15 and their hexidecimal equals using list and dict comprehension

--- Students/CameronDeVos/session05/dict_set_comp.py
+++ Students/CameronDeVos/session05/dict_set_comp.py
@@ -12,3 +12,12 @@ food_pref = {u"name": u"Cameron",
 # Print the dictionary by passing it to a string format method
 print (u"{name} is from {city}, and he likes {cake} cake, {fruit} fruit, "
        "{salad} salad, and {pasta} pasta.\n".format(**food_pref))
+
+# Use list comprehension to build a dictionary of numbers from 0 to 15 and the
+# hexidecimal equivalent
+hexidict1 = dict(zip(range(16), [hex(x) for x in range(16)]))
+print (u"hexidict1 {0}\n".format(hexidict1))
+
+# Do the previous entirely whith a dict comprehension
+hexidict2 = {i: hex(i) for i in range(16)}
+print (u"hexidict2 {0}\n".format(hexidict2))
