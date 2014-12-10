618aa84ad36ab99f2f20695ce763305e84dd95a6
Cameron de Vos <cameron2@outlook.com>
2014-12-10 09:18:37 -0800

    Add unicode to docstring of count_evens function.

--- Students/CameronDeVos/session05/count_evens.py
+++ Students/CameronDeVos/session05/count_evens.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 
 def count_evens(nums):
-    """Takes in a list of numbers and returns the number of evens in list"""
+    u"""Takes in a list of numbers and returns the number of evens in list"""
     return len([num for num in nums if num % 2 == 0])
 
 if __name__ == '__main__':
