375f8949bba9d5b37b34b936e5f67096de5cd807
Cameron de Vos <cameron2@outlook.com>
2014-12-10 08:10:09 -0800

    Add count evens function that returns the number of evens in a list.

---
+++ Students/CameronDeVos/session05/count_evens.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+
+
+def count_evens(nums):
+    """Takes in a list of numbers and returns the number of evens in list"""
+    return len([num for num in nums if num % 2 == 0])
