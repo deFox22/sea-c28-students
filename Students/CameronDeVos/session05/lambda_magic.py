23ccba69337f685a0509dbe50c26785d72ea77c8
Cameron de Vos <cameron2@outlook.com>
2014-12-10 17:43:43 -0800

    Add lambda_magic file and task directions.

---
+++ Students/CameronDeVos/session05/lambda_magic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+# Write a function that returns a list of n functions, such that each one,
+# when called, will return the input value, incremented
+# by an increasing number
+
+# Extra credit: Do it with a list comprehension, instead of a for loop
