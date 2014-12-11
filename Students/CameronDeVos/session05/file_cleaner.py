2a2cef06b3ebbf069c5a3dfc723a9ed005b13345
Cameron de Vos <cameron2@outlook.com>
2014-12-10 18:27:03 -0800

    Add version 2 of overwrite function that uses comprehension.

--- Students/CameronDeVos/session05/file_cleaner.py
+++ Students/CameronDeVos/session05/file_cleaner.py
@@ -16,14 +16,29 @@ def overwrite(filename):
     f.close()
 
 
+def overwrite_comp(filename):
+    u"""Removes the leading and trailing whitespace on each line from file."""
+    f = codecs.open(filename).readlines()
+    clean_lines = [line.strip() + "\n" for line in f]
+    f = codecs.open(filename, 'w').writelines(clean_lines)
+
+
 def rewrite(filename):
     u"""Reads input file and creates new file with whitespace removed"""
     f = codecs.open(filename).readlines()
     clean_lines = map(string.strip, f)
-    f = codecs.open("clean_%s" % filename, "w")
+    f = codecs.open("clean_%s" % filename, 'w')
     for line in clean_lines:
         f.write(line + "\n")
     f.close()
+
+
+def rewrite_comp(filename):
+    u"""Reads input file and creates new file with whitespace removed"""
+    f = codecs.open(filename).readlines()
+    clean_lines = [line.strip() + "\n" for line in f]
+    f = codecs.open("clean_comp_%s" % filename, 'w').writelines(clean_lines)f
+
 
 if __name__ == '__main__':
     while True:
@@ -34,15 +49,15 @@ if __name__ == '__main__':
             break
         filename = sys.argv[1]
         choice = raw_input(u"\nTo clean the inputted file type '1'\n"
-                           "To copy the contents and create a clean_%s "
+                           "To copy the contents and create a clean_comp_%s "
                            "file type '2'\n"
                            "--->  " % filename)
         if choice == "1":
-            overwrite(filename)
+            overwrite_comp(filename)
             print (u"File has been cleaned")
             break
         elif choice == "2":
-            rewrite(filename)
+            rewrite_comp(filename)
             print (u"Copied the file and created"
                    " a new file with clean contents.")
             break
