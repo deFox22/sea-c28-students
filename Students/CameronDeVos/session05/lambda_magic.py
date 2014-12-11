0294e12d9a8319663d53e511b8f33c9c0bbf39bd
Cameron de Vos <cameron2@outlook.com>
2014-12-10 17:44:39 -0800

    Add function_builder function that returns n functions.

--- Students/CameronDeVos/session05/lambda_magic.py
+++ Students/CameronDeVos/session05/lambda_magic.py
@@ -6,3 +6,12 @@ from __future__ import print_function
 # by an increasing number
 
 # Extra credit: Do it with a list comprehension, instead of a for loop
+
+
+def function_builder(n):
+    u"""Return a list of n functions.
+
+    The n functions return the input value, incremented by increasing number
+    """
+    func_list = [lambda x, y=i: x+y for i in range(n)]
+    return func_list
