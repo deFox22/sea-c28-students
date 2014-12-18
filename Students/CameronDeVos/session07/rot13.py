bf7dba31a95e0e36a060cbdea5f9629d65cab80d
Cameron de Vos <cameron2@outlook.com>
2014-12-17 17:57:41 -0800

    Add rot13.py and its test file.

---
+++ Students/CameronDeVos/session07/rot13.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+
+# From the link in the task notes and your tip during class
+# I found the encoding section of the python site
+# to help me easily rot13 some text! Sweetness!
+# Reference: https://docs.python.org/2/library/codecs.html
+import codecs
+
+
+def rot13_encrypt(text):
+    u"""Take a text statement and return it with a rot-13 cypher"""
+    return codecs.encode(text, "rot-13")
