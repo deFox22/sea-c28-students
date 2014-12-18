18b7c32e89bde7420dbdfbb1efa498fd7423a5bc
Cameron de Vos <cameron2@outlook.com>
2014-12-17 17:16:41 -0800

    Add Circle class that takes in radius and has area and diameter as properties.

---
+++ Students/CameronDeVos/session07/circle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import math
+
+
+class Circle(object):
+    u"""Create a circle with a given radius."""
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def set_diameter(self, diameter):
+        self.radius = diameter / 2.0
+
+    def get_diameter(self):
+        return self.radius * 2.0
+
+    def get_area(self):
+        return math.pi*(self.radius**2)
+
+    diameter = property(get_diameter, set_diameter)
+    area = property(get_area)
