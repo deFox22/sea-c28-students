de2e26c32cf0eefe6ab69380b61ace797c38c17e
Cameron de Vos <cameron2@outlook.com>
2014-12-19 08:39:25 -0800

    Add comparison testing between xrange and iterateme_2.

--- Students/CameronDeVos/session08/iterator_1.py
+++ Students/CameronDeVos/session08/iterator_1.py
@@ -51,3 +51,26 @@ class IterateMe_2(object):
             return self.current
         else:
             raise StopIteration
+
+if __name__ == "__main__":
+    print (u"first version")
+    for i in IterateMe_1():
+        print (i)
+
+    print (u"second version")
+    it = IterateMe_2(2, 20, 2)
+    for i in it:
+        if i > 10:
+            break
+        print (i)
+    for i in it:
+        print (i)
+
+    print(u"xrange version")
+    itx = xrange(2, 20, 2)
+    for x in itx:
+        if x > 10:
+            break
+        print (x)
+    for x in itx:
+        print(x)
